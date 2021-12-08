529e472fdec523a5df196a95d8ce36e4a9957104
khoir-roni <khoir.roni@gmail.com>
2021-12-08 10:26:00 +0700

    add consumer provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -16,41 +16,43 @@ class ProductItem extends StatelessWidget {
   // });
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                arguments: product.id);
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        header: Text('\$'),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            color: Theme.of(context).accentColor,
-            onPressed: () => product.toggleFavoriteStatus(),
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
-              color: Theme.of(context).accentColor,
+    // final product = Provider.of<Product>(context);
+    return Consumer<Product>(
+      builder: (context, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
           ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
+          header: Text('\$'),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
               color: Theme.of(context).accentColor,
+              onPressed: () => product.toggleFavoriteStatus(),
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).accentColor,
+              ),
             ),
-            onPressed: null,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: null,
+            ),
           ),
         ),
       ),
