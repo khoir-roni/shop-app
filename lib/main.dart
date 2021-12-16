d5a7c4521be2132f44c6342ed16543e8b623001f
khoir-roni <khoir.roni@gmail.com>
2021-12-16 17:28:37 +0700

    providing an Order object

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/screens/cart_screen.dart';
-import 'package:shop_app/screens/product_detail_screen.dart';
+
+import './providers/orders.dart';
+import './screens/cart_screen.dart';
+import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
@@ -18,6 +20,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(
           value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
         ),
       ],
       child: MaterialApp(
