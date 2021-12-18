bbf8e370c267cf4b196bed9ee9fbc842898fc593
khoir-roni <khoir.roni@gmail.com>
2021-12-18 20:14:01 +0700

    adding and order screen

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../providers/orders.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+  OrderItem(this.order);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyy hh:mm').format(order.dateTime),
+            ),
+            trailing: IconButton(
+              onPressed: null,
+              icon: Icon(Icons.expand_more),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
