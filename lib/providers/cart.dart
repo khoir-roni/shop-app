24fb1ba878bdb90fc1dab9691ecb91829986d0b3
khoir-roni <khoir.roni@gmail.com>
2021-12-20 23:06:25 +0700

    Snackbars & Undoing 'Add to Cart' Actions

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -3,14 +3,14 @@ import 'package:flutter/foundation.dart';
 class CartItem {
   final String id;
   final String title;
-  final int quantity;
   final double price;
+  final int quantity;
 
   CartItem({
     @required this.id,
     @required this.title,
-    @required this.quantity,
     @required this.price,
+    @required this.quantity,
   });
 }
 
@@ -62,6 +62,25 @@ class Cart with ChangeNotifier {
   void removeItem(String productId) {
     _items.remove(productId);
     notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          quantity: existingCartItem.quantity - 1,
+          price: existingCartItem.price,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
   }
 
   void clear() {
