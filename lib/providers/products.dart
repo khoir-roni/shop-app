357b401d7ae1757b585a4b034ae79bb478fc176c
khoir-roni <khoir.roni@gmail.com>
2022-01-02 17:45:20 +0700

    working with Future in dart

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -70,24 +70,28 @@ class Products with ChangeNotifier {
   void addProduct(Product product) {
     const url =
         'https://shop-app-f4326-default-rtdb.asia-southeast1.firebasedatabase.app/products.json';
-    http.post(url,
-        body: json.encode({
-          'title': product.title,
-          'description': product.description,
-          'imageUrl': product.imageUrl,
-          'price': product.price,
-          'isFavorite': product.isFavorite,
-        }));
+    http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'imageUrl': product.imageUrl,
+              'price': product.price,
+              'isFavorite': product.isFavorite,
+            }))
+        .then((response) {
+      print(json.decode(response.body));
+      final newProduct = Product(
+        id: DateTime.now().toString(),
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
     // _items.add(value);
-    final newProduct = Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-    );
-    _items.add(newProduct);
-    notifyListeners();
   }
 
   void updateProduct(String id, Product newProduct) {
