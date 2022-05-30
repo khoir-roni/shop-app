3f3e75618ed820185872f44bd593ce1d6719da9a
khoir-roni <khoir.roni@gmail.com>
2022-05-31 05:53:29 +0700

    add animation builder

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,10 +1,10 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/models/http_exception.dart';
+import '../models/http_exception.dart';
 import '../providers/auth.dart';
 
 enum AuthMode { signUp, logIn }
@@ -97,16 +97,50 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.logIn;
 
-  Map<String, String> _authData = {
+  final Map<String, String> _authData = {
     'email': '',
     'password': '',
   };
-  
-  Void _ShowErrorDialog(String message) {
+
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+
+  AnimationController _controller;
+  Animation<Size> _heighAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+    );
+    _heighAnimation = Tween<Size>(
+      begin: Size(double.infinity, 260),
+      end: Size(double.infinity, 320),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear,
+      ),
+    );
+    _heighAnimation.addListener(() => setState(() {}));
+
+    @override
+    void dispose() {
+      super.dispose();
+      _controller.dispose();
+    }
+  }
+
+  void _showErrorDialog(String message) {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -123,9 +157,6 @@ class _AuthCardState extends State<AuthCard> {
       ),
     );
   }
-
-  var _isLoading = false;
-  final _passwordController = TextEditingController();
 
   Future<void> _submit() async {
     if (!_formKey.currentState.validate()) {
@@ -156,12 +187,12 @@ class _AuthCardState extends State<AuthCard> {
       } else if (error.toString().contains('INVALID_EMAIL')) {
         errorMessage = 'This not valid email address';
       }
-      _ShowErrorDialog(errorMessage);
+      _showErrorDialog(errorMessage);
     } catch (error) {
       const errorMessage = 'Could not authenticate you, please try again later';
 
       print('error : $error');
-      _ShowErrorDialog(errorMessage);
+      _showErrorDialog(errorMessage);
     }
     setState(() {
       _isLoading = false;
@@ -173,10 +204,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.signUp;
       });
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.logIn;
       });
+      _controller.reverse();
     }
   }
 
@@ -188,12 +221,18 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _authMode == AuthMode.signUp ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.signUp ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: const EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heighAnimation,
+        builder: (context, ch) => Container(
+            // height: _authMode == AuthMode.signUp ? 320 : 260,
+            height: _heighAnimation.value.height,
+            constraints: BoxConstraints(
+              // minHeight: _authMode == AuthMode.signUp ? 320 : 260,
+              minHeight: _heighAnimation.value.height,
+            ),
+            width: deviceSize.width * 0.75,
+            padding: const EdgeInsets.all(16.0),
+            child: ch),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -206,7 +245,6 @@ class _AuthCardState extends State<AuthCard> {
                     if (value.isEmpty || !value.contains('@')) {
                       return 'Invalid email!';
                     }
-                    return null;
                     return null;
                   },
                   onSaved: (value) {
